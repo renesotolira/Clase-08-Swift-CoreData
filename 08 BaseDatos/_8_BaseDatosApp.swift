3139f7cf3ef17c786794d98b43647dafa91fca18
René <rene.soto01@iest.edu.mx>
2023-05-21 12:58:34 -0600

    Initial Commit

---
+++ 08 BaseDatos/_8_BaseDatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_BaseDatosApp.swift
+//  08 BaseDatos
+//
+//  Created by Rene SL on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct _8_BaseDatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
